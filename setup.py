e7f8bfde6c008efa84dd1c92cdbcfe7d41c09b8f
Siddhant Thombare <siddhantthombare2121@gmail.com>
2025-04-02 15:13:45 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+## This file can be used to create out model as a package and deploye and use it
+
+
+from setuptools import find_packages,setup
+
+def get_requirenments(file_path:str)->list[str]:
+    '''
+        Installs the dependencies
+    '''
+    HYHEN_E_DOT = '-e .'
+    with open(file_path) as file:
+        require = file.readline()
+        require = [req.replace("\n"," ") for req in require ]
+
+        if HYHEN_E_DOT in require:
+            require.remove(HYHEN_E_DOT)
+    return require
+setup(
+    name="mlproject",
+    author="siddhant",
+    author_email="siddhantthombare2121@gamil.com",
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires= get_requirenments('requirenment.txt')
+)     
